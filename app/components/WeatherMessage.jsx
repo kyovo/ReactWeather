ae277c9fa5391fdb91edf839f46739823676c702
Kyovo Digaw <kyovodigaw@gmail.com>
2017-02-04 12:30:13 +0100

    Add readme file

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 
 var WeatherMessage = ({temp, location}) =>
-<h3>It's it {temp} in {location}!</h3>;
+<h3>It's it {temp} in {location}</h3>;
 
 module.exports = WeatherMessage;
